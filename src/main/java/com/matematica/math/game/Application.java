964a083e5c845dcbf98cf74268c2e9ce1d62f92a
Denis Lindner <lindnerdenis19@gmail.com>
2025-10-22 12:17:07 -0300

    Base do Projeto

---
+++ src/main/java/com/matematica/math/game/Application.java
@@ -0,0 +1,21 @@
+package com.matematica.math.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class Application {
+
+    static Scanner SC =  new Scanner(System.in);
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+        while (true) {
+            System.out.println("=== MENU PRINCIPAL ===");
+            SC.next();
+        }
+	}
+
+}
